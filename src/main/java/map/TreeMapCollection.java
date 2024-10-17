61dadc078e926d3ada13c392901619ed6894bd7a
sahil afrid farookhi <msafarookhi@gmail.com>
2024-10-17 10:33:11 -0500

    add all collection apis and add arraylist implementation

---
+++ src/main/java/map/TreeMapCollection.java
@@ -0,0 +1,10 @@
+package map;
+
+public class TreeMapCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/queue/Deque.java
@@ -0,0 +1,10 @@
+package queue;
+
+public class Deque {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
