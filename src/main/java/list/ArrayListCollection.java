dd65550b707332fbdac641bbe40b43ad924a1ab7
sahil afrid farookhi <msafarookhi@gmail.com>
2024-10-20 15:34:59 -0500

    Add ArrayList

--- src/main/java/list/ArrayListCollection.java
+++ src/main/java/list/ArrayListCollection.java
@@ -1,7 +1,6 @@
 package list;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A resizable array that offers fast random access and is best for frequent read operations.
@@ -10,12 +9,33 @@ import java.util.List;
 public class ArrayListCollection {
 
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<>();
+		
+//		Creating ArrayList
+		ArrayList<String> list = new ArrayList<>();
+		
+//		Adding elements to ArrayList
 		list.add("Alex");
 		list.add("Bob");
 		list.add("Carry");
-		
+		System.out.println("Creating ArrayList:");
 		System.out.println(list);
+		
+//		Iterating through ArrayList
+		System.out.println("\nIterating through ArrayList:");
+		for(String item : list) {
+			System.out.println(item);
+		}
+		
+//		Accessing element from ArrayList
+		String access = list.get(1);
+		System.out.println("\nAccessing an element from ArrayList:");
+		System.out.println(access);
+		
+//		Removing an element from ArrayList
+		list.remove(0);
+		System.out.println("\nArrayList after removing an element:");
+		System.out.println(list);
+		
 	}
 
 }
