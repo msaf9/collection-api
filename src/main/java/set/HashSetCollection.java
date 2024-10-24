4ac58780731b6d237b269d977d6ff7bfe7807301
sahil afrid farookhi <msafarookhi@gmail.com>
2024-10-24 14:27:45 -0500

    add HashSet

--- src/main/java/set/HashSetCollection.java
+++ src/main/java/set/HashSetCollection.java
@@ -1,10 +1,41 @@
 package set;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HashSetCollection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+//		Creating HashSet
+		Set<String> set = new HashSet<>();
+		set.add("NY");
+		set.add("NJ");
+		set.add("IA");
+		set.add("Fl");
+		set.add("MN");
+		set.add("CA");
+		set.add("NJ"); // Duplicate elements are ignored
+		
+//		HashSet elements
+		System.out.println("HashSet contains the following elements");
+		System.out.println(set); // Does not follow any order
+		
+		
+//		Iterating through HashSet
+		System.out.println("\nIterating through HashSet:");
+		for(String item : set) {
+			System.out.println(item);
+		}
+		
+//		Check if an element exists in the HashSet
+		System.out.print("\nWill print true, if CA is found in the HashSet:");
+		System.out.println(set.contains("CA"));
+		
+//		Removing an element from HashSet
+		set.remove("MN");
+		System.out.println("\nHashSet after removing an element:");
+		System.out.println(set);
 	}
 
 }
