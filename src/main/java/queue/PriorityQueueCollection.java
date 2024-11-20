929050591f6d44fe80a0efc8abbd308cfa227709
sahil afrid farookhi <msafarookhi@gmail.com>
2024-11-19 20:40:19 -0600

    WIP: add PriorityQueueCollection.java

--- src/main/java/queue/PriorityQueueCollection.java
+++ src/main/java/queue/PriorityQueueCollection.java
@@ -1,9 +1,27 @@
 package queue;
 
+import java.util.PriorityQueue;
+
 public class PriorityQueueCollection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//		Create a PriorityQueue of integers
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+//		Add elements to PriorityQueue
+		pq.add(10);
+		pq.add(5);
+		pq.add(20);
+		pq.add(15);
+		
+//		Print PriorityQueue
+		System.out.println("PriorityQueue: " + pq);
+		
+//		Print elements in sorted order using poll
+		System.out.print("\nPolling elements in sorted order: ");
+		while(!pq.isEmpty()) {
+			System.out.print(pq.poll() + " ");
+		}
 
 	}
 
