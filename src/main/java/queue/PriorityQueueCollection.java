33a58c37103a992a4905846b19e5bed79f163e8d
sahil afrid farookhi <msafarookhi@gmail.com>
2024-11-25 21:51:36 -0600

    add PriorityQueueCollection.java

--- src/main/java/queue/PriorityQueueCollection.java
+++ src/main/java/queue/PriorityQueueCollection.java
@@ -1,5 +1,8 @@
 package queue;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class PriorityQueueCollection {
@@ -17,12 +20,35 @@ public class PriorityQueueCollection {
 //		Print PriorityQueue
 		System.out.println("PriorityQueue: " + pq);
 		
+//		Remove element 5
+		pq.remove(5);
+		System.out.println("\nPriorityQueue after removing an element (5): " + pq);
+		
+		System.out.println("\nPriorityQueue's peek: " + pq.peek());
+		
+		pq.add(1);
+		
+		List<Integer> list = new ArrayList<>(pq);
+		Collections.sort(list);
+		System.out.println("\nSort elements: " + list);
+		
 //		Print elements in sorted order using poll
 		System.out.print("\nPolling elements in sorted order: ");
 		while(!pq.isEmpty()) {
 			System.out.print(pq.poll() + " ");
 		}
+		
+		/*
+		PriorityQueue: [5, 10, 20, 15]
 
+		PriorityQueue after removing an element (5): [10, 15, 20]
+
+		PriorityQueue's peek: 10
+
+		Sort elements: [1, 10, 15, 20]
+
+		Polling elements in sorted order: 1 10 15 20
+		*/
 	}
 
 }
