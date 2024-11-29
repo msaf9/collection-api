08711b5efea5aa8e30749644fc2d4bfeeedae7d9
sahil afrid farookhi <msafarookhi@gmail.com>
2024-11-28 23:10:45 -0600

    WIP

--- src/main/java/problems/ListProblem3.java
+++ src/main/java/problems/ListProblem3.java
@@ -1,5 +1,8 @@
 package problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Problem: Create a List of integers and rotate the elements to the right by k
  * positions.
@@ -8,7 +11,18 @@ package problems;
 public class ListProblem3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<Integer> list = new ArrayList<>();
+		List<Integer> list2 = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		
+		System.out.println("List before rotation" + list);
+		
+		int k = 1;
+		
 
 	}
 
