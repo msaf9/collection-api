3f7286b3bf64146e905e5fa4ee3d27c2920315a0
sahil afrid farookhi <msafarookhi@gmail.com>
2024-12-13 15:05:16 -0600

    WIP: add MapProblem2 solution

--- src/main/java/problems/MapProblem2.java
+++ src/main/java/problems/MapProblem2.java
@@ -1,5 +1,9 @@
 package problems;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * Problem: Implement a phonebook application using a Map<String, String> where
  * the key is the person’s name, and the value is the phone number. The program
@@ -9,8 +13,29 @@ package problems;
 public class MapProblem2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		Map<String, String> phoneMap = new HashMap<>();
+		
+		System.out.print("Enter your contact name: ");
+		String key = sc.next();
+		System.out.print("Enter your contact number: ");
+		String value = sc.next();
+		
+		phoneMap.put(key, value);
+		
+		System.out.println(phoneMap);
+		
+		System.out.print("Enter your contact name to find: ");
+		String find = sc.next();
+		System.out.println(find + ":" + phoneMap.get(find));
+		
+		System.out.print("Enter your contact name to remove: ");
+		String remove = sc.next();
+		phoneMap.remove(remove);
+		
+		System.out.println(phoneMap);
+		
+		sc.close();
 	}
 
 }
