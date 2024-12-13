b435c9d9c180c6d586274406e0f03f76831bc945
sahil afrid farookhi <msafarookhi@gmail.com>
2024-12-12 21:55:58 -0600

    Add MapProblem4 solution

--- src/main/java/problems/MapProblem4.java
+++ src/main/java/problems/MapProblem4.java
@@ -1,5 +1,8 @@
 package problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Problem: Write a Java program that takes a list of student names and their
  * scores, then returns the name of the student with the highest score using a
@@ -9,8 +12,24 @@ package problems;
 public class MapProblem4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Map<String, Integer> hashMap = new HashMap<>();
+		int high = 0;
+		String name = "";
+		
+		hashMap.put("Alex", 9);
+		hashMap.put("Carry", 1);
+		hashMap.put("Bob", 59);
+		hashMap.put("Dayton", 82);
+		hashMap.put("Elf", 96);
+		
+		for(Map.Entry<String, Integer> entry : hashMap.entrySet()) {
+			if (entry.getValue() >= high) {
+				high = entry.getValue();
+				name = entry.getKey();
+			}
+		}
+		
+		System.out.println(name);
 	}
 
 }
