9e15b74329e213ac307702b6ed88d0bf9df4648b
sahil afrid farookhi <msafarookhi@gmail.com>
2024-12-26 13:19:13 -0800

    wip: work on view all feature

--- src/main/java/problems/MapProblem2.java
+++ src/main/java/problems/MapProblem2.java
@@ -15,28 +15,37 @@ public class MapProblem2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		Map<String, String> phoneMap = new HashMap<>();
-		
-		System.out.println("::Phonebook Menu::");
-		System.out.println("1. Add\n2. Remove\n3. Lookup\n");
-		
-		System.out.print("Select you options from 1 to 3:");
-		int option = sc.nextInt();
-		
-		switch (option) {
-		case 1:
-			add(sc, phoneMap);
-			break;
-		case 2:
-			remove(sc, phoneMap);
-			break;
-		case 3:
-			find(sc, phoneMap);
-			break;
-		default:
-			System.out.println("Invalid option");
-			break;
-		}
-		
+		int option;
+
+		do {
+
+			System.out.println("::Phonebook Menu::");
+			System.out.println("1. Add\n2. Remove\n3. Lookup\n");
+			System.out.println("(0 to exit)\n");
+
+			System.out.print("Select you options from 1 to 3:");
+			option = sc.nextInt();
+
+			switch (option) {
+			case 1:
+				add(sc, phoneMap);
+				break;
+			case 2:
+				remove(sc, phoneMap);
+				break;
+			case 3:
+				find(sc, phoneMap);
+				break;
+			default:
+				if (option != 0) {
+					System.out.println("Invalid option");
+				} else {
+					System.out.println("Exited as the user has selected option 0!");
+				}
+				break;
+			}
+		} while (option != 0);
+
 		sc.close();
 	}
 
@@ -44,13 +53,13 @@ public class MapProblem2 {
 		System.out.print("Enter your contact name to remove: ");
 		String remove = sc.next();
 		phoneMap.remove(remove);
-		System.out.println(phoneMap);
+		System.out.println(phoneMap + "\n");
 	}
 
 	private static void find(Scanner sc, Map<String, String> phoneMap) {
 		System.out.print("Enter your contact name to find: ");
 		String find = sc.next();
-		System.out.println(find + ":" + phoneMap.get(find));
+		System.out.println("\n" + find + ":" + phoneMap.get(find) + "\n");
 	}
 
 	private static void add(Scanner sc, Map<String, String> phoneMap) {
@@ -58,7 +67,7 @@ public class MapProblem2 {
 		String key = sc.next();
 		System.out.print("Enter your contact number: ");
 		String value = sc.next();
-		
+
 		phoneMap.put(key, value);
 		System.out.println(phoneMap);
 	}
